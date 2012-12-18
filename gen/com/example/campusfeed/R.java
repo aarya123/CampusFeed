742cc7f649bb0305e5e8d391f543ad255be39da0
Anubhaw Arya <anubhaw.arya@gmail.com>
2012-12-18 16:50:23 -0500

    JSON support

--- gen/com/example/campusfeed/R.java
+++ gen/com/example/campusfeed/R.java
@@ -14,18 +14,22 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int eventListView=0x7f070000;
+        public static final int menu_settings=0x7f070001;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_event_info=0x7f030000;
+        public static final int activity_main=0x7f030001;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_event_info=0x7f060000;
+        public static final int activity_main=0x7f060001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
+        public static final int title_activity_event_info=0x7f040003;
     }
     public static final class style {
         /** 
--- src/com/example/campusfeed/MainActivity.java
+++ src/com/example/campusfeed/MainActivity.java
@@ -1,25 +1,88 @@
 package com.example.campusfeed;
 
-import android.os.Bundle;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.ActionBar.TabListener;
 import android.app.Activity;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity
 {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState)
+	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
+		ActionBar actionBar = getActionBar();
+		actionBar.addTab(
+				actionBar.newTab().setText("Popular")
+						.setTabListener(new TabListener()
+						{
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu)
-	{
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
-	}
+							@Override
+							public void onTabReselected(Tab tab,
+									FragmentTransaction ft)
+							{
+								// TODO
 
+							}
+
+							@Override
+							public void onTabSelected(Tab tab,
+									FragmentTransaction ft)
+							{
+								// TODO
+
+							}
+
+							@Override
+							public void onTabUnselected(Tab tab,
+									FragmentTransaction ft)
+							{
+								// TODO
+
+							}
+						}), true);
+		ListView listView = (ListView) findViewById(R.id.eventListView);
+		DownloadDataThread downloader = new DownloadDataThread();
+		downloader.start();
+		try
+		{
+			downloader.join(5000);
+		} catch (Exception e)
+		{
+		} finally
+		{
+			ArrayAdapter<String> a = new ArrayAdapter<String>(this,
+					android.R.layout.simple_selectable_list_item,
+					EventOrganizer.getEventNames());
+			listView.setAdapter(a);
+			listView.setOnItemClickListener(new OnItemClickListener()
+			{
+				public void onItemClick(AdapterView<?> parent, View view,
+						int position, long id)
+				{
+					Toast.makeText(getApplicationContext(),
+							"Clicked on item: " + position, Toast.LENGTH_SHORT)
+							.show();
+				}
+
+			});
+		}
+	}
 }
